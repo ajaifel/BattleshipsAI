e3fd2f2e7f7309b7247f4a04140250dbb29558bd
Jack Baxter <jackbaxter007@btinternet.com>
2019-05-21 07:47:37 +0100

    Added - detection of prior shot locations - filling shotlist with checker pattern - next turn updating shot location - huntMode - targeting surrounding tiles - detecting border tiles - logging of hit results to lists for shot detection - win detection (all targets destroyed)
    Testing
    tested with the use cases within the shell, all successful in under 50 turns

---
+++ src/com/company/AI.java
@@ -0,0 +1,135 @@
+package com.company;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class AI {
+
+    boolean hitResult;
+    boolean taskComplete;
+    private int huntModeActive;
+    private int targets;
+    private int hitLocationX;
+    private int hitLocationY;
+    private int size;
+    private Point[] shotList;
+    private Point[] hitList;
+    private Point[] missList;
+    private ArrayList<Point> huntList = new ArrayList<Point>();
+    private int huntCount;
+    private int missCount;
+    private int hitCount;
+    private int huntProgress;
+    private int shotCount;
+
+    public AI(int s, int t) { //initialising all variables
+        hitResult = false;
+        taskComplete = false;
+        size = s;
+        targets = t;
+        missCount = 0;
+        hitCount = 0;
+        huntCount = 0;
+        huntProgress = 0;
+        shotList = new Point[(size*size)/2];
+        hitList = new Point[size*size];
+        missList = new Point[size*size];
+        int c = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if ((i + j) % 2 == 0){
+                    shotList[c] = new Point(i,j); // filling shotList with checkerboard search pattern
+                    c++;
+                }
+            }
+        }
+
+    }
+
+    int getHitLocationX() {
+        return hitLocationX;
+    }
+
+    int getHitLocationY() {
+        return hitLocationY;
+    }
+
+    void nextTurn() {
+        logHitResult(); // adds shotList data to either miss or hit lists
+        if (hitResult) huntMode(); // activates huntmode
+        if (huntModeActive > 0){
+            hitLocationX = huntList.get(huntProgress).x;
+            hitLocationY = huntList.get(huntProgress).y;
+            huntProgress++;
+            huntModeActive--;
+        } else {
+            while (alreadyHit(shotList[shotCount].x, shotList[shotCount].y)) {
+                shotCount++;
+            }
+            hitLocationX = shotList[shotCount].x;
+            hitLocationY = shotList[shotCount].y;
+            shotCount++;
+        }
+    }
+
+    private void huntMode() {                                   // adds surrounding squares to huntList
+        if (!alreadyHit(hitLocationX+1, hitLocationY)){      // tests to see if they're already shot at
+            if (hitLocationX < 9) {                             // doesn't add if they're on a border
+                huntList.add(new Point(hitLocationX + 1, hitLocationY));
+                huntCount++;
+                huntModeActive++;
+            }
+        }
+        if (!alreadyHit(hitLocationX, hitLocationY+1)){
+            if (hitLocationY < 9) {
+                huntList.add(new Point(hitLocationX, hitLocationY + 1));
+                huntCount++;
+                huntModeActive++;
+            }
+        }
+        if (!alreadyHit(hitLocationX-1, hitLocationY)){
+            if (hitLocationX > 0) {
+                huntList.add(new Point(hitLocationX - 1, hitLocationY));
+                huntCount++;
+                huntModeActive++;
+            }
+        }
+        if (!alreadyHit(hitLocationX, hitLocationY-1)){
+            if (hitLocationY > 0) {
+                huntList.add(new Point(hitLocationX, hitLocationY - 1));
+                huntCount++;
+                huntModeActive++;
+            }
+        }
+    }
+
+    private boolean alreadyHit(int x, int y) { // tests locations to see if already shot at prior
+        for (int i = 0; i < hitCount; i++){
+            if (x == hitList[i].x && y == hitList[i].y){
+                return true;
+            }
+        }
+        for (int i = 0; i < missCount; i++) {
+            if (x == missList[i].x && y == missList[i].y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void logHitResult() { // saves hit data to the respective lists
+        if (hitResult == true){
+            targets--;
+            if (targets == 0){
+                taskComplete = true;
+            }
+
+            hitList[hitCount] = new Point(shotList[shotCount].x, shotList[shotCount].y);
+            hitCount++;
+
+        } else {
+            missList[missCount]= new Point(shotList[shotCount].x, shotList[shotCount].y);
+            missCount++;
+        }
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,78 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static int[][] grid = new int[10][10]; // Initialising the shell grid
+
     public static void main(String[] args) {
-	// write your code here
+        selectConfig(); // selecting test case
+
+        AI ai = new AI(10, 17); // creating AI with shell parameters
+        int x;
+        int y;
+        int turns = 0;
+        while (ai.taskComplete == false) { // AI continues until it hits all targets
+            ai.nextTurn(); // rolls the turn forwards, changing the hitLocation selected and updating the Point lists
+            turns++;
+            x = ai.getHitLocationX(); // grabs selected hit location from AI
+            y = ai.getHitLocationY();
+            boolean temp = testHit(x,y); // tests AIs hit selection
+            ai.hitResult = temp;
+        }
+        System.out.println("Task Complete");
+        System.out.printf("%nAll Battleships Sunk in:%n%d turns", turns);
+
+    }
+
+    private static boolean testHit (int x, int y){
+        if (x < 0) {
+            System.out.println("why");
+        }
+        if (grid[x][y] == 1) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+
+    private static void selectConfig(){ // turns 0 to 1 for ship location
+        System.out.println("1. Simple Central Configuration");
+        System.out.println("2. Spread Edge Configuration");
+        System.out.println("3. Complex Random Configuration");
+        System.out.println();
+        System.out.println("Please select one of the above options(1,2,3):");
+        Scanner scanner = new Scanner(System.in);
+        String userIn;
+        while (true){
+            userIn = scanner.next();
+            if (userIn.equals("1")) {
+                grid[2][2] = 1; grid[3][2] = 1; grid[4][2] = 1; grid[5][2] = 1;
+                grid[3][4] = 1; grid[3][5] = 1; grid[3][6] = 1; grid[3][7] = 1; grid[3][8] = 1;
+                grid[5][4] = 1; grid[5][5] = 1;
+                grid[5][7] = 1; grid[6][7] = 1; grid[7][7] = 1;
+                grid[7][3] = 1; grid[7][4] = 1; grid[7][5] = 1;
+                break;
+            } else if (userIn.equals("2")) {
+                grid[0][1] = 1; grid[1][1] = 1; grid[2][1] = 1; grid[3][1] = 1; grid[4][1] = 1;
+                grid[0][4] = 1; grid[0][5] = 1; grid[0][6] = 1;
+                grid[0][8] = 1; grid[0][9] = 1;
+                grid[6][4] = 1; grid[6][5] = 1; grid[6][6] = 1; grid[6][7] = 1;
+                grid[6][0] = 1; grid[7][0] = 1; grid[8][0] = 1;
+                break;
+            } else if (userIn.equals("3")) {
+                grid[0][7] = 1; grid[0][8] = 1;
+                grid[3][2] = 1; grid[4][2] = 1; grid[5][2] = 1; grid[6][2] = 1;
+                grid[8][3] = 1; grid[8][4] = 1; grid[8][5] = 1;
+                grid[7][9] = 1; grid[8][9] = 1; grid[9][9] = 1;
+                grid[3][4] = 1; grid[3][5] = 1; grid[3][6] = 1; grid[3][7] = 1; grid[3][8] = 1;
+                break;
+            } else {
+                System.out.println("That's not a valid input, please input 1, 2 or 3...");
+            }
+        }
     }
 }
